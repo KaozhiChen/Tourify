1c0864d43ed16cbdcf0026a903d9cbbb72717764
KaozhiChen <kylerchen16@gmail.com>
2025-01-01 14:32:10 -0500

    Implement the layout of sign up page

---
+++ Tourify/Views/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Tourify
+//
+//  Created by Kaozhi Chen on 1/1/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
