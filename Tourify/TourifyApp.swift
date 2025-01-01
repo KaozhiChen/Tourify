37b169b2eee60a197d6a63fb38a9199c58f3de6c
KaozhiChen <kylerchen16@gmail.com>
2025-01-01 09:32:16 -0500

    Initial Commit

---
+++ Tourify/TourifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourifyApp.swift
+//  Tourify
+//
+//  Created by Kaozhi Chen on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TourifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
