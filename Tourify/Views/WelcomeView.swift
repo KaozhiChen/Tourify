958d446473bd03cb08176b5b9acbdfa286bbcbba
KaozhiChen <kylerchen16@gmail.com>
2025-01-01 10:42:10 -0500

    Implement welcome page

---
+++ Tourify/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Tourify
+//
+//  Created by Kaozhi Chen on 1/1/25.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
